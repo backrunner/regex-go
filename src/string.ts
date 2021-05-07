2f9caa0067cfc9e5d66ad99ed946353b8b1981c1
BackRunner <dev@backrunner.top>
2021-05-07 21:54:57 +0800

    feat: add chinese tester

--- src/string.ts
+++ src/string.ts
@@ -1,6 +1,12 @@
 import { isStr } from './utils/utils';
-import { emojiRGIRegex } from './utils/patterns';
+import { emojiRGIRegex } from './utils/emoji';
+
+const chineseTester = /[\\u4E00-\\u9FFF]+/g;
 
 export const containsEmoji = (str: string): boolean => {
   return isStr(str) ? emojiRGIRegex.test(str) : false;
+};
+
+export const containsChinese = (str: string): boolean => {
+  return isStr(str) ? chineseTester.test(str) : false;
 };
